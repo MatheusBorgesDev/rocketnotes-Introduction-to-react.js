1f0cc17e5a0df472a1db85b0e8b38370a1908437
matheus <matheusb.dev@gmail.com>
2023-04-19 07:24:52 -0300

    button component created

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./styles"
 
+import { Button } from "../../components/button"
+
 export function Details() {
   return(
     <Container>
       <h1>Hello World!</h1>
       <span>Matheus Borges</span>
+
+      <Button/>
     </Container>
   )
 }
