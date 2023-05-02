8d68d5c8eb917a88954854dc64109d2e0049f9ec
matheus <matheusb.dev@gmail.com>
2023-05-02 07:46:04 -0300

    inative import removed

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,7 +3,6 @@ import { FiMail, FiLock } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/button'
-import { ButtonText } from '../../components/ButtonText'
 
 
 export function SignIn(){
