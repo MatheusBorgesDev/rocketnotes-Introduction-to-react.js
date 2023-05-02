8ee2c062a688d89b90b88689ea668cec68b4b3d6
matheus <matheusb.dev@gmail.com>
2023-05-02 07:43:09 -0300

    SignIn page style finished

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,15 +1,17 @@
-import { Container } from './styles'
+import { Container, Form, Background } from './styles'
 import { FiMail, FiLock } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/button'
+import { ButtonText } from '../../components/ButtonText'
+
 
 export function SignIn(){
   return (
     <Container>
       <Form>
         <h1>Rocket Notes</h1>
-        <p>Aplicação para salvar e gerenciar seus itens úteis</p>
+        <p>Aplicação para salvar e gerenciar seus links úteis</p>
 
         <h2>Faça seu login</h2>
 
@@ -25,17 +27,14 @@ export function SignIn(){
           icon={FiLock}
         />
 
-        <Input 
-          placeholder="E-mail"
-          type="text"
-          icon={FiEmail}
-        />
-
-        <Button>
-          Entrar
-        </Button>
+        <Button title='Entrar' />
         
+        <a href="#">
+          Criar conta
+        </a>
       </Form>
+
+      <Background />
     </Container>
   )
 }
