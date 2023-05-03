8e91fec2d5679fe24653a781f668f6825aefb2e6
matheus <matheusb.dev@gmail.com>
2023-05-03 07:36:48 -0300

    user avatar style added to Profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
-import { Container, Form } from './styles';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
+import { Container, Form, Avatar } from './styles';
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/button'
@@ -14,6 +14,23 @@ export function Profile(){
       </header>
 
         <Form>
+          <Avatar>
+            <img 
+              src="https://github.com/mbslash.png" 
+              alt="Imagem do usuário" 
+            />
+
+            <label htmlFor='avatar'>
+              <FiCamera />
+
+              <input 
+                id='avatar' 
+                type="file" 
+              />
+            </label>
+          </Avatar>
+
+
           <Input 
             placeholder='Nome'
             type='text'
