5bf6aa1add35a80885a92cfa02ddc608d458926f
matheus <matheusb.dev@gmail.com>
2023-08-25 06:40:28 -0300

    bug fix in header image

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,7 @@ export function Header (){
     <Container>
       <Profile to="/profile">
         <img 
-          src="https://github.com/mbslash.png"
+          src="https://github.com/matheusborgesdev.png"
           alt="Foto do usuário"  
         />
         <div>
