109650cbbd71cf70a530141762811ab4e356e5cb
matheus <matheusb.dev@gmail.com>
2023-09-13 02:49:08 -0300

    User avatar bug fix

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/auth'
+import { api } from '../../services/api'
+
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 import { Container, Form, Avatar } from './styles';
-import { useNavigate } from 'react-router-dom';
 
-import { useAuth } from '../../hooks/auth'
-
-import { api } from '../../services/api'
 import avatarPlaceHolder from '../../assets/avatar_placeholder.svg'
+
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/button'
-import { ButtonText } from '../../components/ButtonText'
 
 export function Profile(){
   const { user, updateProfile } = useAuth()
@@ -31,16 +31,16 @@ export function Profile(){
   }
 
   async function handleUpdate(){
-    const user ={
+    const updated ={
       name,
       email,
       password: passwordNew,
       old_password: passwordOld,
     }
 
-    await updateProfile({ user, avatarFile })
+    const userUpdated = Object.assign(user, updated)
 
-    navigate("/")
+    await updateProfile({ user: userUpdated, avatarFile })
   }
 
   async function handleChangeAvatar(event){
